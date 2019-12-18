4a3585993b51e7ca41102e8046d529cdce0de25a
Matt Crook <matt.crook11@gmail.com>
2019-12-18 15:52:24 -0600

    Create array-of-arrays.js

---
+++ array-of-arrays.js
@@ -0,0 +1,33 @@
+// Flatten an array of arrays.
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+// ES2019 solution
+arrays.flat();    // set depth parameter inside .flat() method
+
+
+// Recursion
+function flatten(arrays) {
+  return arrays.reduce((acc, val) => {
+    return acc.concat(val);
+  }, []);
+}
+flatten(arrays);
+// [1, 2, 3, 4, 5, 6];
+
+
+// Working with a huge array, the fastest solution would be:
+let arrays = [[1, 2, 3], [4, 5], [6]];
+const flatten = function(arr, result = []) {
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value);
+    }
+  }
+  return result;
+};
+flatten(arrays);
+// [1, 2, 3, 4, 5, 6];
