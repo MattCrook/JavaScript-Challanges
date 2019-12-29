2fa5dd8901942883fcb6039fcfb17b91f830c1d0
Matt Crook <matt.crook11@gmail.com>
2019-12-29 15:29:00 -0600

    Update array-of-arrays.js

--- array-of-arrays.js
+++ array-of-arrays.js
@@ -3,7 +3,12 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // ES2019 solution
-arrays.flat();    // set depth parameter inside .flat() method
+arrays.flat(3);    // set depth parameter inside .flat() method
+
+
+// functional solution
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+
 
 // Recursion
 function flatten(arrays) {
@@ -11,11 +16,18 @@ function flatten(arrays) {
     return acc.concat(val);
   }, []);
 }
-flatten(arrays);
-// [1, 2, 3, 4, 5, 6];
+flatten(arrays);  // [1, 2, 3, 4, 5, 6];
 
 
-// Working with a huge array, the fastest solution would be:
+// Another way to write using recursion and .reduce() method using arrow function syntax
+const flatArray = arrays.reduce((flat, current) => {
+    return flat.concat(current);
+    }, []);
+
+console.log(flatArray);  // [1,2,3,4,5,6]
+
+
+// Working with a huge array, the fastest solution might be:
 let arrays = [[1, 2, 3], [4, 5], [6]];
 const flatten = function(arr, result = []) {
   for (let i = 0, length = arr.length; i < length; i++) {
@@ -28,5 +40,6 @@ const flatten = function(arr, result = []) {
   }
   return result;
 };
-flatten(arrays);
-// [1, 2, 3, 4, 5, 6];
+flatten(arrays);  // [1, 2, 3, 4, 5, 6];
+
+
